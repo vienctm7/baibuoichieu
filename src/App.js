5a820e47be0b55272b619507d00c0fc1e41970c0
vienctm7 <vienctm7@gmail.com>
2023-04-13 17:30:00 +0700

    add file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Spendmoney from './component/Spendmoney';
+
+function App() {
+  return (
+    <div className="App">
+      <Spendmoney/>
+    </div>
+  );
+}
+
+export default App;
